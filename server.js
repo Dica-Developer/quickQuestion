17af20277a008f54435fe5550f904fc4f88039c1
Jörg Weber <weber_joerg.h@web.de>
2013-08-09 15:48:29 +0200

    *remove execute of pwd command

--- server.js
+++ server.js
@@ -7,18 +7,6 @@ var sys = require('sys');
 var exec = require('child_process').exec;
 var child;
 
-// executes `pwd`
-child = exec("pwd", function (error, stdout, stderr) {
-  sys.print('stdout: ' + stdout);
-  sys.print('stderr: ' + stderr);
-  if (error !== null) {
-    console.log('exec error: ' + error);
-  }
-});
-
-
-
-
 
 var apps = polo();
 var clients = [];
