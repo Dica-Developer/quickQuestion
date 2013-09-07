cd24f856fe82e763c1fe8e269ef3e2a21a2f3283
Jörg Weber <weber_joerg.h@web.de>
2013-09-07 11:10:40 +0200

    * this fixes #40

--- Gruntfile.js
+++ Gruntfile.js
@@ -212,6 +212,21 @@ module.exports = function (grunt) {
     while (!fs.existsSync('tmp/ready')) {}
   });
 
+  grunt.registerTask('setVersion', 'Set version to all needed files', function(version){
+    var config = grunt.config.get(['config']);
+    var appPath = config.app;
+    var resourcesPath = config.resources;
+    var mainPackageJSON = grunt.file.readJSON('package.json');
+    var appPackageJSON = grunt.file.readJSON(appPath + '/package.json');
+    var infoPlistTmp = grunt.file.read(resourcesPath + '/macFiles/Info.plist.tmp', {encoding: 'UTF8'});
+    var infoPlist = grunt.template.process(infoPlistTmp, {data: {version: version}});
+    mainPackageJSON.version = version;
+    appPackageJSON.version = version;
+    grunt.file.write('package.json', JSON.stringify(mainPackageJSON, null, 2), {encoding: 'UTF8'});
+    grunt.file.write(appPath + '/package.json', JSON.stringify(appPackageJSON, null, 2), {encoding: 'UTF8'});
+    grunt.file.write(resourcesPath + '/macFiles/Info.plist', infoPlist, {encoding: 'UTF8'});
+  });
+
   grunt.registerTask('dist-linux', [
     'jshint',
     'clean:dist',
