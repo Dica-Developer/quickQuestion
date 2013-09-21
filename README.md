95331965f3841afe1be7042ea1f4bd902e4db947
Martin Schaaf <mschaaf@datameer.com>
2013-09-21 22:13:16 +0200

    * add license file and add license notices to source files

--- README.md
+++ README.md
@@ -33,3 +33,22 @@ To build a distribution you have to run the following commands depending on your
 The built app can then be found in the folder:
 
     ./dist/
+
+## License ##
+
+Quick Question - Communication and Collaboration tool.
+Copyright (C) 2013  Dica-Developer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
--- app/js/db.js
+++ app/js/db.js
@@ -1,3 +1,21 @@
+/*
+Quick Question - Communication and Collaboration tool.
+Copyright (C) 2013  Dica-Developer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*global window*/
 var fs = require('fs');
 var sys = require('sys');
 var events = require('events');
@@ -13,17 +31,21 @@ function Db(name) {
   this.storingPath = nwGui.App.dataPath + '/' + name + '.json';
 
   var _this = this;
-  fs.readFile(this.storingPath, {encoding: 'UTF8'}, function(error, data){
-    if(!error && data){
+  fs.readFile(this.storingPath, {
+    encoding: 'UTF8'
+  }, function (error, data) {
+    if (!error && data) {
       _this.query = TAFFY(JSON.parse(data));
-    }else{
+    } else {
       _this.query = TAFFY();
     }
     _this.emit('ready');
   });
   this.save = function () {
     process.stdout.write('Persisting ' + this.name + ' database' + os.EOL);
-    fs.writeFileSync(this.storingPath, JSON.stringify(this.query().get()), {encoding: 'UTF8'});
+    fs.writeFileSync(this.storingPath, JSON.stringify(this.query().get()), {
+      encoding: 'UTF8'
+    });
   };
 }
 sys.inherits(Db, events.EventEmitter);
