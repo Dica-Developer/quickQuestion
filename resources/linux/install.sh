ff4c69b8cf41c9423507e1c97a2da1c7aedb0db7
Martin Schaaf <martin@mascha.me>
2013-10-11 22:18:41 +0200

    * install icons on linux builds

--- resources/linux/install.sh
+++ resources/linux/install.sh
@@ -7,3 +7,5 @@ cp dist/node-webkit /usr/lib/quickquestion/
 cp dist/qq /usr/bin/
 
 cp resources/linux/quick-question.desktop /usr/share/applications/
+cp resources/icons/QQ_Colored.svg /usr/share/pixmaps/
+
