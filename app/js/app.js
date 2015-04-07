eec54dd0f1a5e9b6b49ea665cb768eaf5a38d498
Martin Schaaf <mschaaf@datameer.com>
2015-04-07 19:50:46 +0200

    * remove obsolete comment

--- app/js/app.js
+++ app/js/app.js
@@ -83,7 +83,6 @@ server.on('updateClients', function () {
   var i = 0;
   this.clients.sort(sortByHostName);
   for (i = 0; i < this.clients.length; i++) {
-    // TODO encode hostname and address
     content = content + '<li data-icon="check"><a href="#">' + encodeHtml(this.clients[i].nickname + ':' + this.clients[i].hostname + ' (' + this.clients[i].address + ')') + '</a></li>';
   }
   $('ul[name="collaboratorListView"]').each(function () {
